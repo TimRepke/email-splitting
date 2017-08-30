7b2609c084345234e0e79375ee2575bf7196df09
Tim Repke <tim@repke.eu>
2017-08-30 18:19:24 +0200

    add Jangada original code

--- scripts/Jangada/features.py
+++ scripts/Jangada/features.py
@@ -1,5 +1,11 @@
 import re
 
+"""Module for generating a feature representation of each line in an email
+
+This code is a Python translation of the Jangada source code in scripts/Jangada/original/src/jangada/SigFilePredictor.java
+Structure and variable names are more or less kept to keep the code comparable.
+"""
+
 # constants
 tail_lines = 10
 
@@ -28,7 +34,7 @@ def detect_from_name(input_str, test_line):
 
 
 def start_with_same_initial_punct_characters(line1, line2):
-    if len(line1) > 0 and len(line2)>0:
+    if len(line1) > 0 and len(line2) > 0:
         char1 = line1[0]
         char2 = line2[0]
         if char1 in "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~":
@@ -39,14 +45,14 @@ def start_with_same_initial_punct_characters(line1, line2):
 def indent_number(line):
     if len(line) == 0:
         return 0
-    return len([c for c in line if c=="\t"])
+    return len([c for c in line if c == "\t"])
 
 
 def indent_percentage(line):
     num = indent_number(line)
     if num == 0:
         return 0.0
-    return num/len(line)
+    return num / len(line)
 
 
 def punctuation_number(line):
@@ -57,7 +63,7 @@ def punctuation_percentage(line):
     num = punctuation_number(line)
     if len(line) == 0:
         return 0.0
-    return num/len(line)
+    return num / len(line)
 
 
 def word_character_number(line):
@@ -99,14 +105,14 @@ def mail2features(mail):
             features['lastbutbutoneL'] = 1
 
         # header feature
-        if re.search("^\\s?\\s?[\\w|\\-]+\\:", line):
-            if re.search("^\\s?\\s?(http|HTTP|Phone|PHONE|phone|email|EMAIL|Internet|INTERNET|internet)+\\:", line):
+        if re.search("^\s?\s?(\w|-)+:", line):
+            if re.search("^\s?\s?(http|HTTP|Phone|PHONE|phone|email|EMAIL|Internet|INTERNET|internet)+:", line):
                 features['header'] = 1
             if (size - i) < tail_lines:
                 features['closeheader'] = 1
 
         # blank line features
-        regex_blank_line = "^[\\s|\\t]*$"
+        regex_blank_line = r"^\s*$"
         if i > 1:
             if re.search(regex_blank_line, mail[i - 2]):
                 features['prevprevblankL'] = 1
@@ -135,7 +141,7 @@ def mail2features(mail):
                     features['closenextnextblanL'] = 1
 
         # sig marker feature
-        regex_sig_marker = "^[\\s]*---*[\\s]*$"
+        regex_sig_marker = r"^\s*---*\s*$"
         if i > 0:
             if re.search(regex_sig_marker, mail[i - 1]):
                 features['prevsigMarker'] = 1
@@ -152,7 +158,7 @@ def mail2features(mail):
                     features['closenextsigMarker'] = 1
 
         # trueSigMarker - post-addition
-        regex_true_sig_marker = "^[\\s]?[\\s]?---?[\\s]*$"
+        regex_true_sig_marker = r"^\s?\s?---?\s*$"
         if i > 3:
             if re.search(regex_true_sig_marker, mail[i - 4]):
                 features['prevprevprevprevtruesigMarker'] = 1
@@ -194,7 +200,7 @@ def mail2features(mail):
                     features['nextnextnextclosetruesigMarker'] = 1
 
         # other markers features
-        regex_other_markers = "^[\\s]*([\\*]|#|[\\+]|[\\^]|-|[\\~]|[\\&]|[////]|[\\$]|_|[\\!]|[\\/]|[\\%]|[\\:]|[\\=]){10,}[\\s]*$"
+        regex_other_markers = r"^\s*(?:\*|#|\+|\^|-|~|&|/|\$|_|!|/|%|:|=){10,}\s*$"
         if i > 0:
             if re.search(regex_other_markers, mail[i - 1]):
                 features['prevotherMarkers'] = 1
@@ -211,7 +217,7 @@ def mail2features(mail):
                     features['closenextotherMarkers'] = 1
 
         # special works feature
-        regex_special_works = "Dept\\.|University|Corp\\.|Corporations?|College|Ave\\.|Laboratory|[D|d]isclaimer|Division|Professor|Laboratories|Institutes?|Services|Engineering|Director|Sciences?|Address|Fax|Office|Mobile|Phone|Manager|Street|St\\.|Avenue"
+        regex_special_works = r"Dept\.|University|Corp\.|Corporations?|College|Ave\.|Laboratory|[D|d]isclaimer|Division|Professor|Laboratories|Institutes?|Services|Engineering|Director|Sciences?|Address|Fax|Office|Mobile|Phone|Manager|Street|St\.|Avenue"
         if i > 0:
             if re.search(regex_special_works, mail[i - 1]):
                 features['prevspecWords'] = 1
@@ -228,7 +234,7 @@ def mail2features(mail):
                     features['closenextspecWords'] = 1
 
         # email feature
-        regex_email = "[^(\\<|\\>)][\\w|\\+|\\.|\\_|\\-]+\\@[\\w|\\-|\\_|\\.]+\\.[a-zA-z]{2,5}[^(\\<|\\>)]"
+        regex_email = r"[^<>](?:\w|\+|\.|_|-]+@(?:\w|-|_|\.)+\.[a-zA-z]{2,5}[^<>]"
         if i > 0:
             if re.search(regex_email, mail[i - 1]):
                 features['prevemail'] = 1
@@ -243,13 +249,13 @@ def mail2features(mail):
                 features['nextemail'] = 1
                 if (size - i) < tail_lines:
                     features['closenextemail'] = 1
-        if re.search("[^(\\<|\\>)][(\\w|\\+|\\_|\\-)]+\\@[(\\w|\\-|\\_)]+[\\.][a-zA-z]{2,5}", mail[i]):
+        if re.search(r"[^<>](?:\w|\+|_|-)+@(?:\w|-|_)+\.[a-zA-z]{2,5}", mail[i]):
             features['emailB'] = 1
             if (size - i) < tail_lines:
                 features['closeemailB'] = 1
 
         # URL feature
-        regex_url = "[\\s](http\\:\\/\\/)*(www|web|w3)*(\\w[\\w|\\-]+)\\.(\\w[\\w|\\-]+)\\.(\\w[\\w|\\-]+)*[\\w]+"
+        regex_url = r"\s?(?:http://)*(?:www|web|w3)*\w(?:\w|-)+\.\w(?:\w|-)+\.\w(?:\w|-)+*\w+"
         if i > 0:
             if re.search(regex_url, mail[i - 1]):
                 features['preveurl'] = 1
@@ -266,7 +272,7 @@ def mail2features(mail):
                     features['closenexturl'] = 1
 
         # phone
-        regex_phone = "(\\-?\\d)*\\d\\d\\s?\\-?\\s?\\d\\d\\d\\d"
+        regex_phone = r"(?:-?\d)*\d\d\s?-?\s?\d\d\d\d"
         if i > 0:
             if re.search(regex_phone, mail[i - 1]):
                 features['prevephone'] = 1
@@ -277,7 +283,7 @@ def mail2features(mail):
                 features['nextphone'] = 1
 
         # names like Vitor R.Carvalho or John F.Kennedy
-        if re.search("[A-Z][a-z]+\\s\\s?[A-Z][\\.]?\\s\\s?[A-Z][a-z]+", mail[i]):
+        if re.search(r"[A-Z][a-z]+\s\s?[A-Z]\.?\s\s?[A-Z][a-z]+", mail[i]):
             features['namepat'] = 1
             if (size - i) < tail_lines:
                 features['closenamepat'] = 1
@@ -301,7 +307,7 @@ def mail2features(mail):
                         features['closenextfromL'] = 1
 
         # reply symbol
-        regex_reply_symbol = "^\\>.*"
+        regex_reply_symbol = "^\s*>.*"
         if i > 0:
             if re.search(regex_reply_symbol, mail[i - 1]):
                 features['prevreplySymbol'] = 1
@@ -312,7 +318,7 @@ def mail2features(mail):
                 features['nextreplySymbol'] = 1
 
         # other reply symbol
-        regex_other_reply_symbol = "^[\\=|\\:|\\#|\\:|\\-|\\+|\\&|\\%|\\}]\\s*\\w+.*"
+        regex_other_reply_symbol = r"^(?:=|:|#|-|\+|&|%|\})\s*\w+.*"
         if i > 0:
             if re.search(regex_other_reply_symbol, mail[i - 1]):
                 features['prevotherreplySymbol'] = 1
@@ -323,7 +329,7 @@ def mail2features(mail):
                 features['nextotherreplySymbol'] = 1
 
         # punct starting and followed by ">"
-        regex_punct_gr = "^\\p{Punct}{1,2}\\>.*"
+        regex_punct_gr = r"^[!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{1,2}>.*"
         if i > 0:
             if re.search(regex_punct_gr, mail[i - 1]):
                 features['prevpunct'] = 1
@@ -334,8 +340,8 @@ def mail2features(mail):
                 features['nextpunct'] = 1
 
         # writes and wrote features
-        regex_writes = " writes:$"
-        regex_wrote = " wrote:$"
+        regex_writes = r" writes:\s*$"
+        regex_wrote = r" wrote:\s*$"
         if i > 0:
             if re.search(regex_writes, mail[i - 1]):
                 features['prevwrites'] = 1
@@ -352,11 +358,11 @@ def mail2features(mail):
                 features['nextwrote'] = 1
 
         # same initial punct characters
-        if i > 0 and start_with_same_initial_punct_characters(mail[i], mail[i-1]):
+        if i > 0 and start_with_same_initial_punct_characters(mail[i], mail[i - 1]):
             features['prevsicline'] = 1
             if (size - i) < tail_lines:
                 features['closeprevsicline'] = 1
-        if i < (size-1) and start_with_same_initial_punct_characters(mail[i], mail[i+1]):
+        if i < (size - 1) and start_with_same_initial_punct_characters(mail[i], mail[i + 1]):
             features['nextsicline'] = 1
             if (size - i) < tail_lines:
                 features['closenextsicline'] = 1
@@ -376,7 +382,7 @@ def mail2features(mail):
             if (size - i) < tail_lines:
                 features['closeindentTri'] = 1
         if i > 0:
-            ddd = indent_number(mail[i-1])
+            ddd = indent_number(mail[i - 1])
             if ddd == 1:
                 features['previndentUni'] = 1
                 if (size - i) < tail_lines:
@@ -389,7 +395,7 @@ def mail2features(mail):
                 features['previndentTri'] = 1
                 if (size - i) < tail_lines:
                     features['closeprevindentTri'] = 1
-        if i < (size-1):
+        if i < (size - 1):
             ddd = indent_number(mail[i + 1])
             if ddd == 1:
                 features['nextindentUni'] = 1
@@ -427,7 +433,7 @@ def mail2features(mail):
             if (size - i) < tail_lines:
                 features['closepunctPerc90'] = 1
         if i > 0:
-            temp = punctuation_percentage(mail[i-1])
+            temp = punctuation_percentage(mail[i - 1])
             if temp > 0.2:
                 features['punctPerc20prev'] = 1
                 if (size - i) < tail_lines:
@@ -444,7 +450,7 @@ def mail2features(mail):
                 features['punctPerc90prev'] = 1
                 if (size - i) < tail_lines:
                     features['closepunctPerc90prev'] = 1
-        if i < (size -1):
+        if i < (size - 1):
             temp = punctuation_percentage(mail[i + 1])
             if temp > 0.2:
                 features['punctPerc20next'] = 1
